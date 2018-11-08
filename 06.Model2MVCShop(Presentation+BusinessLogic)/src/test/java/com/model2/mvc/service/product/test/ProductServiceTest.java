bddba05cf72e0f3583a53b16e362984962f17581
SangaChoi <tkddk92@naver.com>
2018-11-08 16:26:39 +0900

    Product 완료

---
+++ 06.Model2MVCShop(Presentation+BusinessLogic)/src/test/java/com/model2/mvc/service/product/test/ProductServiceTest.java
@@ -0,0 +1,147 @@
+package com.model2.mvc.service.product.test;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.model2.mvc.common.Search;
+import com.model2.mvc.service.domain.Product;
+import com.model2.mvc.service.product.ProductService;
+
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration	(locations = {	"classpath:config/context-common.xml",
+		"classpath:config/context-aspect.xml",
+		"classpath:config/context-mybatis.xml",
+		"classpath:config/context-transaction.xml" })
+public class ProductServiceTest {
+	
+	@Autowired
+	@Qualifier("productServiceImpl")
+	private ProductService productService;
+	
+	//@Test
+	public void testAddProduct() throws Exception{
+		
+		Product product=new Product();
+		product.setProdName("카밀");
+		product.setProdDetail("핸드크림");
+		product.setManuDate("20011212");
+		product.setPrice(5000);
+		product.setFileName("수정파일네임");
+		
+		productService.addProduct(product);
+		
+	}
+	
+	//@Test
+	public void testGetProduct() throws Exception{
+		
+		Product product=new Product();
+		product=productService.getProduct(10161);
+		
+		System.out.println(product);
+	}
+	
+	//@Test
+	public void testUpdateProduct() throws Exception{
+		
+		Product product=productService.getProduct(10161);
+		Assert.assertNotNull(product);
+		
+		product.setProdName("카밀수정");
+		product.setProdDetail("카밀디테일수정");
+		product.setManuDate("20050505");
+		product.setPrice(6000);
+		product.setFileName("카밀수정파일");
+		
+		productService.updateProduct(product);
+		
+		product=productService.getProduct(10161);
+		System.out.println(product);
+	}
+	
+	//@Test
+	public void testGetProductListAll() throws Exception{
+		
+		Search search = new Search();
+	 	search.setCurrentPage(1);
+	 	search.setPageSize(3);
+	 	Map<String,Object> map = productService.getProductList(search);
+	
+	 	List<Object> list = (List<Object>)map.get("list");
+	 	Assert.assertEquals(3, list.size());
+	 	
+		//==> console 확인
+	 	System.out.println(list);
+	 	
+	 	Integer totalCount = (Integer)map.get("totalCount");
+	 	System.out.println(totalCount);
+	 	
+	}
+	
+	//@Test
+	public void testGetProductListByProdNo() throws Exception{
+		
+		Search search = new Search();
+	 	search.setCurrentPage(1);
+	 	search.setPageSize(3);
+	 	search.setSearchCondition("0");
+	 	search.setSearchKeyword("10146");
+	 	Map<String,Object> map = productService.getProductList(search);
+		
+	 	List<Object> list = (List<Object>)map.get("list");
+	 	
+	 	System.out.println(list);
+	 	
+	 	Integer totalCount = (Integer)map.get("totalCount");
+	 	System.out.println(totalCount);
+		
+	}
+	
+	//@Test
+	 public void testGetProductListByProdName() throws Exception{
+		
+		Search search = new Search();
+	 	search.setCurrentPage(1);
+	 	search.setPageSize(3);
+	 	search.setSearchCondition("1");
+	 	search.setSearchKeyword("자");
+	 	Map<String,Object> map = productService.getProductList(search);
+		
+	 	List<Object> list = (List<Object>)map.get("list");
+	 	
+	 	System.out.println(list);
+	 	
+	 	Integer totalCount = (Integer)map.get("totalCount");
+	 	System.out.println(totalCount);
+		
+	}
+	 
+	@Test
+		 public void testGetProductListByPrice() throws Exception{
+			
+			Search search = new Search();
+		 	search.setCurrentPage(1);
+		 	search.setPageSize(3);
+		 	search.setSearchCondition("2");
+		 	search.setSearchKeyword("1111");
+		 	Map<String,Object> map = productService.getProductList(search);
+			
+		 	List<Object> list = (List<Object>)map.get("list");
+		 	
+		 	System.out.println(list);
+		 	
+		 	Integer totalCount = (Integer)map.get("totalCount");
+		 	System.out.println(totalCount);
+			
+		}
+}
